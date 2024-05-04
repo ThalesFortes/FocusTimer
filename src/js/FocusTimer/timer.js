58abfe007f6fcbf81eb30ce6d2f0bbc5f996b4e2
ThalesFortes <thales_pro@outlook.com>
2024-05-04 19:34:53 -0300

    Adicionando sons aos botoes e musicas

--- src/js/FocusTimer/timer.js
+++ src/js/FocusTimer/timer.js
@@ -1,6 +1,7 @@
 import state from "./state.js"
 import * as elements from "./elements.js"
 import { reset } from "./actions.js"
+import { kitchenTimer } from "./sounds.js"
 
 
 export function countdown(){
@@ -19,6 +20,7 @@ export function countdown(){
 
   if (minutes < 0 ){
     reset()
+    kitchenTimer.play()
     return
   }
 
